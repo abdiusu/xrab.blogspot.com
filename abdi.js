90eb62b0ba7b8160cbade064d81bd1c441923b41
abdi syahputra harahap <44754630+abdiusu@users.noreply.github.com>
2019-04-10 13:21:34 +0700

    Update abdi.js

--- abdi.js
+++ abdi.js
@@ -18,7 +18,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://xrab.blogspot.com/2019/04/tes.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*5)+ 1);
+var xxx=null;
+if(x=="1"){xxx="tes.html"}
+if(x=="2"){xxx="tes.html"}
+if(x=="3"){xxx="tes.html"}
+if(x=="4"){xxx="tes.html"}
+if(x=="5"){xxx="tes.html"}a_to_ve[i].href="https://xrab.blogspot.com/2019/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
